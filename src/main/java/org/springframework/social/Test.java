95921e790832efb26bde6d125fd41f14918dc481
vbochenin <vbochenin@gmail.com>
2015-09-01 18:09:36 +0300

    pom.xml added

---
+++ src/main/java/org/springframework/social/Test.java
@@ -0,0 +1,13 @@
+package org.springframework.social;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author vbochenin
+ * @since 01/09/2015.
+ */
+public class Test {
+    private final static Logger log = LoggerFactory.getLogger(Test.class);
+
+}
